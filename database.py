628a050cf8ce36f7702ad92cce22583e53a9c463
NEKOgrile <willemcornil@gmail.com>
2025-10-06 10:28:26 +0200

    Update database connection pour avoir une identifiant externe

--- database.py
+++ database.py
@@ -1,8 +1,19 @@
 # database.py
+import json
 from sqlmodel import SQLModel, create_engine, Session
 
-# Connexion à la base MariaDB locale
-DATABASE_URL = "mysql+mysqlconnector://root:E5EAAC93E5@127.0.0.1:3306/project_API_TISSEA"
+# Charger les identifiants depuis identifiant.json
+with open("identifiant.json") as f:
+    config = json.load(f)
+
+user = config["user"]
+password = config["password"]
+host = config["host"]
+port = config["port"]
+database = config["database"]
+
+# Construire l'URL de connexion
+DATABASE_URL = f"mysql+mysqlconnector://{user}:{password}@{host}:{port}/{database}"
 
 # Création du moteur SQLAlchemy
 engine = create_engine(DATABASE_URL, echo=True)
